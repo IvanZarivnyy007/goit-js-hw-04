eaa5593265c021f572bf5bae4ee3cbf251ceaebe
IvanZarivnyy007 <144140704+IvanZarivnyy007@users.noreply.github.com>
2023-12-07 19:55:45 +0100

    11
    11

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+function isEnoughCapacity(products, containerSize) {
+    const values = Object.values(products) 
+    let sum = 0;
+    for(let i = 0; i < values.length; i++ ){
+        sum =+ value
+
+    }
+    return sum
+
+}
+
+
+
+
+
+
+console.log(
+isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
